f12a0c51ec1fb11fad8316b83b553c5c3ed21dfc
Ronak Duggar <ronakduggar202@gmail.com>
2025-03-18 09:39:58 +0530

    Dynamic ID path added

--- REST/index.js
+++ REST/index.js
@@ -7,7 +7,7 @@ const PORT = 5001;
 app.get("/users", (req, res) => {
     const html = `
       <ul>
-          ${users.map((user) => `<li>${user.id}</li>`).join("")}
+          ${users.map((user) => `<li>${user.email}</li>`).join("")}
       </ul>`;
     return res.send(html);
   });
@@ -16,6 +16,13 @@ app.get("/users", (req, res) => {
 app.get("/api/users", (req, res) => {
   res.json(users);
 });
+
+app.get("/api/users/:id",(req,res)=>{
+    const id = Number(req.params.id);
+    const user = users.find((user)=>user.id === id);
+    return res.json(user);
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is Running on PORT: ${PORT}`);
