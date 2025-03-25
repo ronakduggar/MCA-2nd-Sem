b655643153493321bf4f4116e3c150dbadfc238f
Ronak Duggar <ronakduggar202@gmail.com>
2025-03-25 09:50:43 +0530

    Common Routes for GET request

--- REST/index.js
+++ REST/index.js
@@ -17,12 +17,22 @@ app.get("/api/users", (req, res) => {
   res.json(users);
 });
 
-app.get("/api/users/:id",(req,res)=>{
+app.route("/api/users/:id")
+  .get((req,res)=>{
     const id = Number(req.params.id);
     const user = users.find((user)=>user.id === id);
     return res.json(user);
-})
+  })
+  .put((req,res)=>{
+    return res.json({status:"pending"});
+  })
+  .delete((req,res)=>{
+    return res.json({status:"pending"});
+  })
 
+app.post("/api/users",(req,res)=>{
+  return res.json({status:"pending"});
+})
 
 app.listen(PORT, () => {
   console.log(`Server is Running on PORT: ${PORT}`);
