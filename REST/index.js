cf1472990da698add3395a72af2b179ff186acac
Ronak Duggar <ronakduggar202@gmail.com>
2025-11-11 12:25:18 +0530

    Checking Path

--- REST/index.js
+++ REST/index.js
@@ -1,27 +1,86 @@
 const express = require("express");
-const app = express();
-const PORT = 4000;
 const users = require("./data.json");
+const fs = require("fs");
+const app = express();
+const mongoose = require("mongoose");
+const { type } = require("os");
+const PORT = 8000;
 
-app.get("/users",(req,res)=>{
-    const html = `<ul>
-        ${users.map((user)=> `<li>${user.email}</li>`).join("")}
-    </ul>`;
-    return res.send(html);
-})
+//Connection
+mongoose.connect("mongodb://127.0.0.1:27017").then(() => {
+  console.log("MongoDB is Connected");
+});
 
-app.get("/api/users", (req, res)=>{
-     return res.json(users);
-})
+//Schema
+const userSchema = mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
 
-app.get("/api/users/:id", (req,res)=>{
+//Model
+const userModel = mongoose.model("user", userSchema);
+
+//MiddleWare
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  console.log("This is a Middleware 1");
+  res.send({'msg': "You are not Authorized to acces this Server"});
+  next();
+});
+
+app.use((req, res) => {
+  console.log("This is a Middleware 2");
+  res.end("You are not Authorized to acces this Server");
+});
+
+//HTML DOCUMENT
+app.get("/users", (req, res) => {
+  const html = `
+      <ul>
+          ${users.map((user) => `<li>${user.email}</li>`).join("")}
+      </ul>`;
+  return res.send(html);
+});
+
+//REST API
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
     const id = Number(req.params.id);
     const user = users.find((user) => user.id === id);
-    console.log(typeof(user));
     return res.json(user);
-})
+  })
+  .patch((req, res) => {
+    return res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    return res.json({ status: "This is pending" });
+  });
 
+app.post("/api/users", (req, res) => {
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("./data.json", JSON.stringify(users), (err, data) => {
+    res.json({ status: "The user has been Created" });
+  });
+});
 
-app.listen(PORT,()=>{
-    console.log(`Server is Running on PORT: ${PORT}`);
-} )
+app.listen(PORT, () => {
+  console.log(`Server is Running on PORT: ${PORT}`);
+});
